2c6ff95bdcf39f148bf2f153e5647bc46f51738d
David Patiño <david.patino1@utp.edu.co>
2024-10-12 00:28:58 -0500

    fixed commits

--- src/app/dashboard/history/[edit-response]/page.tsx
+++ src/app/dashboard/history/[edit-response]/page.tsx
@@ -3,7 +3,7 @@ import OutputSection from '@/components/Content/OutputSection'
 
 function EditResponse() {
   return (
-    <div>
+    <div className='m-3 p-3 shadow-lg rounded-md'>
         <OutputSection aiOutput='' />
     </div>
   )
