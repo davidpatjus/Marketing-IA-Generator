905f34598a506f76f6d3b77caec0ec5a35af6519
David Patiño <david.patino1@utp.edu.co>
2024-10-11 22:35:59 -0500

    OutputSection in History v1.10

--- src/app/dashboard/history/layout.tsx
+++ src/app/dashboard/history/layout.tsx
@@ -1,22 +1,33 @@
-'use client'
+'use client';
+
 import OutputSection from '@/components/Content/OutputSection';
 import { SelectedAIResponseContext } from '@/app/(context)/SelectedAIResponse';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 interface HistoryLayoutProps {
     children: React.ReactNode;
 }
 
-
 const HistoryLayout: React.FC<HistoryLayoutProps> = ({ children }) => {
     const { selectedAIResponse } = useContext(SelectedAIResponseContext);
+    const [isClient, setIsClient] = useState(false);
+
+    // Se asegura de que el componente esté montado en el cliente
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return null; // Retorna null si no está en el cliente
+    }
+
     return (
-            <main>
-                {children}
-                <div className='p-3 m-3 bg-white rounded-lg shadow-lg'>
-                <OutputSection aiOutput={selectedAIResponse || ''} />                
-                </div>
-            </main>
+        <main>
+            {children}
+            <div className='p-3 m-3 bg-white rounded-lg shadow-lg'>
+                <OutputSection aiOutput={selectedAIResponse || ''} />
+            </div>
+        </main>
     );
 };
 
