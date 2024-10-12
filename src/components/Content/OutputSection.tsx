a21e8c409b7ea12373d7780873997d372cdae739
David Patiño <david.patino1@utp.edu.co>
2024-10-11 22:19:54 -0500

    OutputSection in History v1.5

--- src/components/Content/OutputSection.tsx
+++ src/components/Content/OutputSection.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Button } from '../ui/button';
@@ -11,10 +10,9 @@ interface PROPS {
 
 function OutputSection({ aiOutput }: PROPS) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const editorRef = useRef<any>(null); // Mantén el tipo any si no tienes el tipo correcto de TOAST UI Editor
+  const editorRef = useRef<any>(null); 
   const [isClient, setIsClient] = useState(false);
 
-  // Este hook solo se ejecutará cuando el componente esté montado en el cliente
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -26,8 +24,22 @@ function OutputSection({ aiOutput }: PROPS) {
     }
   }, [aiOutput, isClient]);
 
+  const handleCopy = () => {
+    if (isClient) {
+      const copyToClipboard = async (text: string) => {
+        try {
+          await navigator.clipboard.writeText(text);
+          alert('Texto copiado al portapapeles!');
+        } catch (error) {
+          console.error('Error al copiar al portapapeles:', error);
+        }
+      };
+
+      copyToClipboard(editorRef.current.getInstance().getMarkdown());
+    }
+  };
+
   if (!isClient) {
-    // Mientras no estemos en el cliente, retornamos un fragmento vacío o un loading
     return null;
   }
 
@@ -35,22 +47,7 @@ function OutputSection({ aiOutput }: PROPS) {
     <div className="bg-white shadow-lg border rounded-lg">
       <div className="flex justify-between items-center p-5">
         <h2 className="font-medium text-lg">Tu Resultado</h2>
-        <Button
-          className="flex gap-2"
-          onClick={() => {
-            const copyToClipboard = async (text: string) => {
-              try {
-                await navigator.clipboard.writeText(text);
-                alert('Texto copiado al portapapeles!');
-              } catch (error) {
-                console.error('Error al copiar al portapapeles:', error);
-              }
-            };
-          
-
-            copyToClipboard(editorRef.current.getInstance().getMarkdown());
-          }}
-        >
+        <Button className="flex gap-2" onClick={handleCopy}>
           <Copy className="w-4 h-4" /> Copiar
         </Button>
       </div>
