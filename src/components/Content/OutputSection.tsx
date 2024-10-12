b4f787a632a93767a1bd52ecf83f76e096d5f513
David Patiño <david.patino1@utp.edu.co>
2024-10-11 22:38:15 -0500

    OutputSection in History v1.11

--- src/components/Content/OutputSection.tsx
+++ src/components/Content/OutputSection.tsx
@@ -1,4 +1,4 @@
-'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef, useState } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
@@ -10,49 +10,41 @@ interface PROPS {
 }
 
 function OutputSection({ aiOutput }: PROPS) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const editorRef = useRef<any>(null); 
+  const editorRef = useRef<any>(null);
   const [isClient, setIsClient] = useState(false);
   const [markdownContent, setMarkdownContent] = useState('');
   const [copyTriggered, setCopyTriggered] = useState(false);
 
-  // Se asegura de que el componente esté montado en el cliente
   useEffect(() => {
     setIsClient(true);
   }, []);
 
-  // Actualiza el contenido del editor cuando se actualiza `aiOutput`
   useEffect(() => {
     if (isClient && editorRef.current) {
       const editorInstance = editorRef.current.getInstance();
       editorInstance.setMarkdown(aiOutput);
-      setMarkdownContent(aiOutput); // Guarda el contenido actual del markdown
+      setMarkdownContent(aiOutput);
     }
   }, [aiOutput, isClient]);
 
-  // Maneja el copiado al portapapeles
   useEffect(() => {
-    if (copyTriggered && markdownContent) {
+    if (copyTriggered && markdownContent && isClient) {
       const copyToClipboard = async () => {
-        if (typeof navigator !== 'undefined' && navigator.clipboard) {
-          try {
-            await navigator.clipboard.writeText(markdownContent);
-            alert('Texto copiado al portapapeles!');
-          } catch (error) {
-            console.error('Error al copiar al portapapeles:', error);
-          }
-        } else {
-          console.error("La API de portapapeles no está disponible.");
+        try {
+          await navigator.clipboard.writeText(markdownContent);
+          alert('Texto copiado al portapapeles!');
+        } catch (error) {
+          console.error('Error al copiar al portapapeles:', error);
         }
       };
 
       copyToClipboard();
-      setCopyTriggered(false); // Restablecer el estado después de copiar
+      setCopyTriggered(false);
     }
-  }, [copyTriggered, markdownContent]);
+  }, [copyTriggered, markdownContent, isClient]);
 
   const handleCopy = () => {
-    setCopyTriggered(true); // Indicar que se ha activado la acción de copia
+    setCopyTriggered(true);
   };
 
   if (!isClient) {
